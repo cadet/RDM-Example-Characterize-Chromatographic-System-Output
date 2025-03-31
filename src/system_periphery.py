9ac4c86c942a9c6fc4aa68ea95c8c7db242a8df9
r.jaepel <ronald.jaepel@gmail.com>
2025-03-31 09:08:19 +0200

    Run large optimizations

---
+++ src/system_periphery.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Model System Periphery
+#
+# Show Foto
+#
+# Show Abstraction
+#
+# Show Model
+#
+#
+# Fit Parameters
+#
+# Missing:
+# - Data
+# - Fotos
